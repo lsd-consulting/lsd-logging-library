bce63517ed7c1ef645d238ec16e02da1e5420b32
Lukasz Gryzbon <lukasz.gryzbon@gmail.com>
2023-08-14 12:35:31 +0100

    fix: Revert the static import

--- src/main/kotlin/lsd/logging/Logger.kt
+++ src/main/kotlin/lsd/logging/Logger.kt
@@ -2,8 +2,7 @@ package lsd.logging
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.lang.Thread.currentThread
 
 inline fun <reified T> T.log(): Logger = LoggerFactory.getLogger(T::class.java)
 
-fun log(): Logger = LoggerFactory.getLogger(currentThread().stackTrace[2].methodName)
+fun log(): Logger = LoggerFactory.getLogger(Thread.currentThread().stackTrace[2].methodName)
